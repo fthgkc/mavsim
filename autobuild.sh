995d6f7ad351a4962dd1909f6812d44c2946ebbe
James Goppert <jgoppert@w7.hsl.dynalias.com>
2011-01-18 18:34:01 -0500

    Added in source build mode.

--- autobuild.sh
+++ autobuild.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
-rm -rf build
-mkdir build
-cd build
-cmake ..
-make
+
+PS3='Please enter your choice: '
+LIST="in_source_build install_build grab_debian_dependencies END"
+select OPT in $LIST
+do
+	if [ $OPT = "in_source_build" ] &> /dev/null
+	then
+		echo you chose in source build
+		rm -rf build
+		mkdir build
+		cd build
+		cmake -DIN_SRC_BUILD:bool=TRUE ..
+		make
+		exit 0
+	elif [ $OPT = "install_build" ] &> /dev/null
+	then
+		echo you chose install build
+		rm -rf build
+		mkdir build
+		cd build
+		cmake ..
+		make
+		exit 0
+	elif [ $OPT = "grab_debian_dependencies" ] &> /dev/null
+	then
+		echo you chose to install debian dependencies
+		sudo apt-get install cmake libqt4-dev simgear-dev liblapack-dev libboost-all-dev libopenscenegraph-dev gfortran
+		exit 0
+
+	elif [ $OPT = "END" ] &> /dev/null
+	then
+		exit 0
+	fi
+done
--- oooark-toolbox/demos/blockDemos.sce
+++ oooark-toolbox/demos/blockDemos.sce
@@ -1,10 +1,20 @@
-n=x_choose(['JSBSim Backside Autopilot Design';'JSBSim Trim';'Visualization Test'],'oooark block demos')
+n=x_choose([
+'EasyStar Backside Autopilot Design';
+'EasyStar Trim';
+'EasyStar Hardware in the Loop (Sensor Level)';
+'EasyStar Hardware in the Loop (State Level)';
+'Visualization Test';
+],'oooark block demos')
 
 if (n==1)
 	scicos(oooarkPath+'demos/JSBSimBackside.cos')
 elseif (n==2)
 	scicos(oooarkPath+'demos/JSBSimTrim.cos')
 elseif (n==3)
+	scicos(oooarkPath+'demos/JSBSimMavLinkHilState.cos')
+elseif (n==4)
+	scicos(oooarkPath+'demos/JSBSimMavLinkHilSensor.cos')
+elseif (n==5)
 	scicos(oooarkPath+'demos/VisTest.cos')
 else
 	disp('unknown block')
