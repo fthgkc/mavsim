e76e68008a0d45afb7d3635f1ca61ddadb2a3e01
James Goppert <jgoppert@w7.hsl.dynalias.com>
2011-01-19 15:45:40 -0500

    Fix for source distribution.

--- autobuild.sh
+++ autobuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PS3='Please enter your choice: '
-LIST="in_source_build install_build grab_debian_dependencies package_source package remake END"
+LIST="in_source_build install_build grab_debian_dependencies package_source package remake clean END"
 echo 
 echo in_source_build: is used for development and you can start the scicoslab toolbox by typing scicoslab in the oooark source directory
 echo install_build: is used for building before final installation to the system.
@@ -9,6 +9,7 @@ echo grab_debian_dependencies: installs all the required packages for debian bas
 echo remake: calls make again after project has been configured as install or in source build
 echo package_source: creates a source package for distribution
 echo package: creates binary packages for distribution
+echo clean: removes the build directory 
 
 echo
 select OPT in $LIST
@@ -16,20 +17,12 @@ do
 	if [ $OPT = "in_source_build" ] &> /dev/null
 	then
 		echo you chose in source build
-		rm -rf build
-		mkdir build
-		cd build
-		cmake -DIN_SRC_BUILD:bool=TRUE ..
-		make
+		mkdir -p build && cd build && cmake .. && cmake -DIN_SRC_BUILD:bool=TRUE .. && make -j8
 		exit 0
 	elif [ $OPT = "install_build" ] &> /dev/null
 	then
 		echo you chose install build
-		rm -rf build
-		mkdir build
-		cd build
-		cmake ..
-		make
+		mkdir -p build && cd build && cmake .. && make -j8
 		exit 0
 	elif [ $OPT = "grab_debian_dependencies" ] &> /dev/null
 	then
@@ -48,13 +41,19 @@ do
 	elif [ $OPT = "package_source" ] &> /dev/null
 	then
 		echo you chose to package the source
-		cd build && make package_source
+		mkdir -p build && cd build && cmake .. && make package_source
 		exit 0
 
 	elif [ $OPT = "package" ] &> /dev/null
 	then
 		echo you chose to package the binary
-		cd build && make package
+		mkdir -p build && cd build && cmake .. && make package
+		exit 0
+
+	elif [ $OPT = "clean" ] &> /dev/null
+	then
+		echo you chose to clean the build 
+		rm -rf build
 		exit 0
 
 	elif [ $OPT = "END" ] &> /dev/null
