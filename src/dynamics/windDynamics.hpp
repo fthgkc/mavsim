8a1302e79dc402023d4180a817edbec58343e56d
Nicholas Metaxas <nmetaxas@w3.hsl.dynalias.com>
2011-03-03 10:47:34 -1000

    Generated cpp for quad/ wind/ body dynamics blocks.

---
+++ src/dynamics/windDynamics.hpp
@@ -0,0 +1,10 @@
+d_x_wind(0,0) = -((cosAlpha*cosBeta*sinTheta+(-sinPhi*sinBeta-cosPhi*sinAlpha*cosBeta)*cosTheta)*g*m-F_b_T_(1,0)*sinBeta+(-F_b_T_(2,0)*sinAlpha-F_b_T_(0,0)*cosAlpha)*cosBeta-F_w_A_(0,0))/m;
+d_x_wind(1,0) = ((sinAlpha*sinTheta+cosPhi*cosAlpha*cosTheta)*g*m-F_b_T_(0,0)*sinAlpha+F_b_T_(2,0)*cosAlpha+F_w_A_(2,0))/(cosBeta*m*Vt);
+d_x_wind(2,0) = cosPhi*wy-sinPhi*wz;
+d_x_wind(3,0) = (Jxy*Jz+Jxz*Jyz)*(-wy*(Jz*wz-Jyz*wy-Jxz*wx)+wz*(-Jyz*wz+Jy*wy-Jxy*wx)+M_b_T_(0,0)+M_b_A_(0,0))/(Jx*(Jy*Jz-Jyz)-Jxy*(Jxy*Jz+Jxz*Jyz)-Jxz*(Jxy*Jyz+Jxz*Jy))+(Jx*Jz-JxzJxz)*(wx*(Jz*wz-Jyz*wy-Jxz*wx)-wz*(-Jxz*wz-Jxy*wy+Jx*wx)+M_b_T_(1,0)+M_b_A_(1,0))/(Jx*(Jy*Jz-Jyz)-Jxy*(Jxy*Jz+Jxz*Jyz)-Jxz*(Jxy*Jyz+Jxz*Jy))+(Jx*Jyz+Jxy*Jxz)*(-wx*(-Jyz*wz+Jy*wy-Jxy*wx)+wy*(-Jxz*wz-Jxy*wy+Jx*wx)+M_b_T_(2,0)+M_b_A_(2,0))/(Jx*(Jy*Jz-Jyz)-Jxy*(Jxy*Jz+Jxz*Jyz)-Jxz*(Jxy*Jyz+Jxz*Jy));
+d_x_wind(4,0) = sin(theta-alpha)*Vt;
+d_x_wind(5,0) = ((cosAlpha*sinBeta*sinTheta+(sinPhi*cosBeta-cosPhi*sinAlpha*sinBeta)*cosTheta)*g*m+(-F_b_T_(2,0)*sinAlpha-F_b_T_(0,0)*cosAlpha)*sinBeta+F_b_T_(1,0)*cosBeta+F_w_A_(1,0))/(m*Vt);
+d_x_wind(6,0) = tanTheta*(cosPhi*wz+sinPhi*wy)+wx;
+d_x_wind(7,0) = (Jy*Jz-Jyz)*(-wy*(Jz*wz-Jyz*wy-Jxz*wx)+wz*(-Jyz*wz+Jy*wy-Jxy*wx)+M_b_T_(0,0)+M_b_A_(0,0))/(Jx*(Jy*Jz-Jyz)-Jxy*(Jxy*Jz+Jxz*Jyz)-Jxz*(Jxy*Jyz+Jxz*Jy))+(Jxy*Jz+Jxz*Jyz)*(wx*(Jz*wz-Jyz*wy-Jxz*wx)-wz*(-Jxz*wz-Jxy*wy+Jx*wx)+M_b_T_(1,0)+M_b_A_(1,0))/(Jx*(Jy*Jz-Jyz)-Jxy*(Jxy*Jz+Jxz*Jyz)-Jxz*(Jxy*Jyz+Jxz*Jy))+(Jxy*Jyz+Jxz*Jy)*(-wx*(-Jyz*wz+Jy*wy-Jxy*wx)+wy*(-Jxz*wz-Jxy*wy+Jx*wx)+M_b_T_(2,0)+M_b_A_(2,0))/(Jx*(Jy*Jz-Jyz)-Jxy*(Jxy*Jz+Jxz*Jyz)-Jxz*(Jxy*Jyz+Jxz*Jy));
+d_x_wind(8,0) = (cosPhi*wz+sinPhi*wy)/cosTheta;
+d_x_wind(9,0) = (Jxy*Jyz+Jxz*Jy)*(-wy*(Jz*wz-Jyz*wy-Jxz*wx)+wz*(-Jyz*wz+Jy*wy-Jxy*wx)+M_b_T_(0,0)+M_b_A_(0,0))/(Jx*(Jy*Jz-Jyz)-Jxy*(Jxy*Jz+Jxz*Jyz)-Jxz*(Jxy*Jyz+Jxz*Jy))+(Jx*Jyz+Jxy*Jxz)*(wx*(Jz*wz-Jyz*wy-Jxz*wx)-wz*(-Jxz*wz-Jxy*wy+Jx*wx)+M_b_T_(1,0)+M_b_A_(1,0))/(Jx*(Jy*Jz-Jyz)-Jxy*(Jxy*Jz+Jxz*Jyz)-Jxz*(Jxy*Jyz+Jxz*Jy))+(Jx*Jy-JxyJxy)*(-wx*(-Jyz*wz+Jy*wy-Jxy*wx)+wy*(-Jxz*wz-Jxy*wy+Jx*wx)+M_b_T_(2,0)+M_b_A_(2,0))/(Jx*(Jy*Jz-Jyz)-Jxy*(Jxy*Jz+Jxz*Jyz)-Jxz*(Jxy*Jyz+Jxz*Jy));
--- theory/generateCode.sh
+++ theory/generateCode.sh
@@ -27,8 +27,9 @@ else
 	exit
 fi
 
-srcPath=../src/navigation/
+navPath=../src/navigation/
 sciPath=.
+dynPath=../src/dynamics/
 
 cat code/ins_dynamics_f.f90 | sed \
 	-e "s/$/;/g" \
@@ -56,7 +57,7 @@ cat code/ins_dynamics_f.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_dynamics_f.hpp
+	> ${navPath}/ins_dynamics_f.hpp
 
 cat code/ins_dynamics_f_att.f90 | sed \
 	-e "s/$/;/g" \
@@ -84,7 +85,7 @@ cat code/ins_dynamics_f_att.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_dynamics_f_att.hpp
+	> ${navPath}/ins_dynamics_f_att.hpp
 
 cat code/ins_dynamics_f_vp.f90 | sed \
 	-e "s/$/;/g" \
@@ -112,7 +113,7 @@ cat code/ins_dynamics_f_vp.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_dynamics_f_vp.hpp
+	> ${navPath}/ins_dynamics_f_vp.hpp
 
 cat code/ins_error_dynamics_F.f90 | sed \
 	-e "s/$/;/g" \
@@ -142,7 +143,7 @@ cat code/ins_error_dynamics_F.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_error_dynamics_F.hpp
+	> ${navPath}/ins_error_dynamics_F.hpp
 
 cat code/ins_error_dynamics_F_att.f90 | sed \
 	-e "s/$/;/g" \
@@ -172,7 +173,7 @@ cat code/ins_error_dynamics_F_att.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_error_dynamics_F_att.hpp
+	> ${navPath}/ins_error_dynamics_F_att.hpp
 
 cat code/ins_error_dynamics_F_vp.f90 | sed \
 	-e "s/$/;/g" \
@@ -202,7 +203,7 @@ cat code/ins_error_dynamics_F_vp.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_error_dynamics_F_vp.hpp
+	> ${navPath}/ins_error_dynamics_F_vp.hpp
 
 cat code/ins_error_dynamics_G.f90 | sed \
 	-e "s/$/;/g" \
@@ -233,7 +234,7 @@ cat code/ins_error_dynamics_G.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_error_dynamics_G.hpp
+	> ${navPath}/ins_error_dynamics_G.hpp
 
 cat code/ins_error_dynamics_G_att.f90 | sed \
 	-e "s/$/;/g" \
@@ -245,7 +246,7 @@ cat code/ins_error_dynamics_G_att.f90 | sed \
 	-e "s/secL\**2/secLsecL/g" \
 	-e "s/R\*\*2/RR/g" \
 	-e "s/cosL\*\*2/cosLcosL/g" \
-	> ${srcPath}/ins_error_dynamics_G_att.hpp
+	> ${navPath}/ins_error_dynamics_G_att.hpp
 
 cat code/ins_error_dynamics_G_vp.f90 | sed \
 	-e "s/$/;/g" \
@@ -276,7 +277,7 @@ cat code/ins_error_dynamics_G_vp.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_error_dynamics_G_vp.hpp
+	> ${navPath}/ins_error_dynamics_G_vp.hpp
 
 cat code/ins_H_mag.f90 | sed \
 	-e "s/$/;/g" \
@@ -307,7 +308,7 @@ cat code/ins_H_mag.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_H_mag.hpp
+	> ${navPath}/ins_H_mag.hpp
 
 cat code/ins_H_mag_att.f90 | sed \
 	-e "s/$/;/g" \
@@ -338,7 +339,7 @@ cat code/ins_H_mag_att.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_H_mag_att.hpp
+	> ${navPath}/ins_H_mag_att.hpp
 
 cat code/ins_R_mag_n.f90 | sed \
 	-e "s/$/;/g" \
@@ -367,7 +368,7 @@ cat code/ins_R_mag_n.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_R_mag_n.hpp
+	> ${navPath}/ins_R_mag_n.hpp
 
 cat code/ins_H_gps.f90 | sed \
 	-e "s/$/;/g" \
@@ -391,7 +392,7 @@ cat code/ins_H_gps.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_H_gps.hpp
+	> ${navPath}/ins_H_gps.hpp
 
 cat code/ins_H_gps_vp.f90 | sed \
 	-e "s/$/;/g" \
@@ -415,7 +416,7 @@ cat code/ins_H_gps_vp.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_H_gps_vp.hpp
+	> ${navPath}/ins_H_gps_vp.hpp
 
 cat code/ins_R_gps.f90 | sed \
 	-e "s/$/;/g" \
@@ -439,7 +440,7 @@ cat code/ins_R_gps.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_R_gps.hpp
+	> ${navPath}/ins_R_gps.hpp
 
 cat code/ins_C_nb_quat.f90 | sed \
 	-e "s/$/;/g" \
@@ -464,7 +465,7 @@ cat code/ins_C_nb_quat.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_C_nb_quat.hpp
+	> ${navPath}/ins_C_nb_quat.hpp
 
 cat code/ins_C_nb_euler.f90 | sed \
 	-e "s/$/;/g" \
@@ -489,7 +490,7 @@ cat code/ins_C_nb_euler.f90 | sed \
 	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
 	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
 	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
-	> ${srcPath}/ins_C_nb_euler.hpp
+	> ${navPath}/ins_C_nb_euler.hpp
 
 cat code/quad_wind_dynamics.f90 | sed \
 	-e "s/$/;/g" \
@@ -508,3 +509,87 @@ cat code/quad_body_dynamics.f90 | sed \
 	-e "s/%//g" \
 	-e "s/pi/%pi/g" \
 	> ${sciPath}/quad_body_dynamics.sci
+
+cat code/quadForceMoments.f90 | sed \
+	-e "s/$/;/g" \
+	-e "/^.*) = 0;$/d" \
+	-e "s/%//g" \
+	-e "s/(1,/(0,/g" -e "s/,1)/,0)/g" \
+	-e "s/(2,/(1,/g" -e "s/,2)/,1)/g" \
+	-e "s/(3,/(2,/g" -e "s/,3)/,2)/g" \
+	-e "s/(4,/(3,/g" -e "s/,4)/,3)/g" \
+	-e "s/(5,/(4,/g" -e "s/,5)/,4)/g" \
+	-e "s/(6,/(5,/g" -e "s/,6)/,5)/g" \
+	-e "s/(7,/(6,/g" -e "s/,7)/,6)/g" \
+	-e "s/(8,/(7,/g" -e "s/,8)/,7)/g" \
+	-e "s/(9,/(8,/g" -e "s/,9)/,8)/g" \
+	-e "s/(10,/(9,/g" -e "s/,10)/,9)/g" \
+	-e "s/(11,/(10,/g" -e "s/,11)/,10)/g" \
+	-e "s/(12,/(11,/g" -e "s/,12)/,11)/g" \
+	-e "s/(13,/(12,/g" -e "s/,13)/,12)/g" \
+	-e "s/(14,/(13,/g" -e "s/,14)/,13)/g" \
+	-e "s/(15,/(14,/g" -e "s/,15)/,14)/g" \
+	-e "s/(16,/(15,/g" -e "s/,16)/,15)/g" \
+	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
+	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
+	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
+    -e "s/pi\*\*3/piPiPi/g" -e "s/pi\*\*2/piPi/g" -e "s/batVolt\*\*2/batVoltBatVolt/g" -e "s/KV\*\*2/KVKV/g" \
+	-e "s/rBlade\*\*4/rBladeRBladeRBladeRBlade/g" -e "s/rBlade\*\*5/rBladeRBladeRBladeRBladeRBlade/g" -e "s/Vt\*\*2/VtVt/g" -e "s/(Q)/Q/g" \
+    -e "s/(T)/T/g" -e "s/alpha\*\*2/alphaAlpha/g" \
+	> ${dynPath}/quadForceMoments.hpp
+
+
+cat code/d_x_wind.f90 | sed \
+	-e "s/$/;/g" \
+	-e "/^.*) = 0;$/d" \
+	-e "s/%//g" \
+	-e "s/(1,/(0,/g" -e "s/,1)/,0)/g" \
+	-e "s/(2,/(1,/g" -e "s/,2)/,1)/g" \
+	-e "s/(3,/(2,/g" -e "s/,3)/,2)/g" \
+	-e "s/(4,/(3,/g" -e "s/,4)/,3)/g" \
+	-e "s/(5,/(4,/g" -e "s/,5)/,4)/g" \
+	-e "s/(6,/(5,/g" -e "s/,6)/,5)/g" \
+	-e "s/(7,/(6,/g" -e "s/,7)/,6)/g" \
+	-e "s/(8,/(7,/g" -e "s/,8)/,7)/g" \
+	-e "s/(9,/(8,/g" -e "s/,9)/,8)/g" \
+	-e "s/(10,/(9,/g" -e "s/,10)/,9)/g" \
+	-e "s/(11,/(10,/g" -e "s/,11)/,10)/g" \
+	-e "s/(12,/(11,/g" -e "s/,12)/,11)/g" \
+	-e "s/(13,/(12,/g" -e "s/,13)/,12)/g" \
+	-e "s/(14,/(13,/g" -e "s/,14)/,13)/g" \
+	-e "s/(15,/(14,/g" -e "s/,15)/,14)/g" \
+	-e "s/(16,/(15,/g" -e "s/,16)/,15)/g" \
+	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
+	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
+	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
+	-e "s/Jxy\*\*2/JxyJxy/g" -e "s/Jxz\*\*2/JxzJxz/g" -e "s/Jyz\*\*2/Jyz/g" -e "s/KV\*\*2/KVKV/g" \
+    -e "s/(phi)/Phi/g" -e "s/(theta)/Theta/g" -e "s/(alpha)/Alpha/g" -e "s/(beta)/Beta/g" \
+	> ${dynPath}/windDynamics.hpp
+
+
+cat code/dynamicsBodyFrame.f90 | sed \
+	-e "s/$/;/g" \
+	-e "/^.*) = 0;$/d" \
+	-e "s/%//g" \
+	-e "s/(1,/(0,/g" -e "s/,1)/,0)/g" \
+	-e "s/(2,/(1,/g" -e "s/,2)/,1)/g" \
+	-e "s/(3,/(2,/g" -e "s/,3)/,2)/g" \
+	-e "s/(4,/(3,/g" -e "s/,4)/,3)/g" \
+	-e "s/(5,/(4,/g" -e "s/,5)/,4)/g" \
+	-e "s/(6,/(5,/g" -e "s/,6)/,5)/g" \
+	-e "s/(7,/(6,/g" -e "s/,7)/,6)/g" \
+	-e "s/(8,/(7,/g" -e "s/,8)/,7)/g" \
+	-e "s/(9,/(8,/g" -e "s/,9)/,8)/g" \
+	-e "s/(10,/(9,/g" -e "s/,10)/,9)/g" \
+	-e "s/(11,/(10,/g" -e "s/,11)/,10)/g" \
+	-e "s/(12,/(11,/g" -e "s/,12)/,11)/g" \
+	-e "s/(13,/(12,/g" -e "s/,13)/,12)/g" \
+	-e "s/(14,/(13,/g" -e "s/,14)/,13)/g" \
+	-e "s/(15,/(14,/g" -e "s/,15)/,14)/g" \
+	-e "s/(16,/(15,/g" -e "s/,16)/,15)/g" \
+	-e "s/(17,/(16,/g" -e "s/,17)/,16)/g" \
+	-e "s/(18,/(17,/g" -e "s/,18)/,17)/g" \
+	-e "s/(19,/(18,/g" -e "s/,19)/,18)/g" \
+	-e "s/Jxy\*\*2/JxyJxy/g" -e "s/Jxz\*\*2/JxzJxz/g" -e "s/Jyz\*\*2/Jyz/g" -e "s/KV\*\*2/KVKV/g" \
+	-e "s/(phi)/Phi/g" -e "s/(theta)/Theta/g" -e "s/(alpha)/Alpha/g" -e "s/(beta)/Beta/g" \
+	> ${dynPath}/dynamicsBodyFrame.hpp
