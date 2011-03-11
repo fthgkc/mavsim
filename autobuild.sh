598f959be5240888a1592bcc6c3b3b8926276af0
James Goppert <jgoppert@w7.hsl.dynalias.com>
2011-03-11 13:00:57 -0500

    Added fPIC flags to cmake build.

--- autobuild.sh
+++ autobuild.sh
@@ -4,13 +4,13 @@ PS3='Please enter your choice: '
 LIST="in_source_build install_build grab_debian_dependencies package_source package remake clean END"
 MAKEARGS="-j8"
 echo 
-echo in_source_build: is used for development and you can start the scicoslab toolbox by typing scicoslab in the mavsim source directory
-echo install_build: is used for building before final installation to the system.
-echo grab_debian_dependencies: installs all the required packages for debian based systems \(ubuntu maverick/ debian squeeze,lenny\)
-echo remake: calls make again after project has been configured as install or in source build
-echo package_source: creates a source package for distribution
-echo package: creates binary packages for distribution
-echo clean: removes the build directory 
+echo 1\)in_source_build: is used for development and you can start the scicoslab toolbox by typing scicoslab in the mavsim source directory
+echo 2.\)install_build: is used for building before final installation to the system.
+echo 3.\)grab_debian_dependencies: installs all the required packages for debian based systems \(ubuntu maverick/ debian squeeze,lenny\)
+echo 4.\)package: creates binary packages for distribution
+echo 5.\)package_source: creates a source package for distribution
+echo 6.\)remake: calls make again after project has been configured as install or in source build
+echo 7.\)clean: removes the build directory 
 
 echo
 select OPT in $LIST
@@ -18,7 +18,9 @@ do
 	if [ $OPT = "in_source_build" ] &> /dev/null
 	then
 		echo you chose in source build
-		mkdir -p build && cd build && cmake -DIN_SRC_BUILD:bool=TRUE .. && make $MAKEARGS
+		mkdir -p build && cd build && cmake -DCXXFLAGS="-fPIC" \
+			-DCPPFLAGS="-fPIC" -DCFLAGS="-fPIC" \
+			-DIN_SRC_BUILD:bool=TRUE .. && make $MAKEARGS
 		exit 0
 	elif [ $OPT = "install_build" ] &> /dev/null
 	then
